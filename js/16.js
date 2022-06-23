2896d1fef593e30b300dd0c793e3991fd1ce7730
roggergolden <77041053@certus.edu.pe>
2022-06-22 22:00:10 -0500

    cambios java

--- js/16.js
+++ js/16.js
@@ -1,2 +1,24 @@
 // Funciones 
 
+// Declaración de Función
+
+function sumar() {
+    console.log(10 + 10);
+}
+
+sumar()
+
+// Expresión de la función 
+
+const sumar2 = function() {
+    console.log(3 + 3);
+}
+
+sumar2();
+
+
+//IIFE
+
+(function() {
+    console.log("Esto es una función");
+})();
